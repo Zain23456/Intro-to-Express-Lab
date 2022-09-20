aa8cbc5dc6cf44a06fdb1b97ca0015f9bd5bdb40
Zain Ahmed <z.zain.ahmed@gmail.com>
2022-09-19 20:34:31 -0400

    create students data

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Marlin', student: false, _id: 123456},
+  {name: 'Dory', student: false, _id: 123457},
+  {name: 'Nemo', student: true, _id: 123458},
+  {name: 'Mr. Ray', student: false, _id: 123459},
+  {name: 'Tad', student: true, _id: 123460},
+  {name: 'Pearl', student: true, _id: 123461},
+  {name: 'Sheldon', student: true, _id: 123462}
+]
